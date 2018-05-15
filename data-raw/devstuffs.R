68f2acbea3d748485db27a681bc14ae2b59daaa6
Colin Fay <colin@thinkr.fr>
2018-05-15 14:51:18 +0200

    first commit 👶

---
+++ data-raw/devstuffs.R
@@ -0,0 +1,2 @@
+colin::fill_desc("fryingpane", "Serve Datasets in RStudio Connection Pane",
+                 "Use the RStudio Connection Pane to serve the data from a package.", repo = "fryingpane")
